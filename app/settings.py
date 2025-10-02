aefda2f4376137a9f772fc8ff203f032acd79d72
Eduardo <epa.edu@gmail.com>
2025-10-02 11:09:50 -0300

    Configuração do Potgres

--- app/settings.py
+++ app/settings.py
@@ -111,6 +111,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'jiyuu',
+        #'USER': 'postgres',
+        #'PASSWORD': '@eparaujo2025',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
     }
 }
 
