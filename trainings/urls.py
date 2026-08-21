9f1f2eb945da15797f5e34e3b3cbd30c97f66b8d
Eduardo <epa.edu@gmail.com>
2026-08-21 17:49:18 -0300

    Correção no processo de marcação de presença no treino, endpoint que o app estava chamando estave em conflito

--- trainings/urls.py
+++ trainings/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("api/v1/attendance/register/", views.attendance_register_view, name="attendance_register"),
     path("api/v1/trainings/checkin/create-session/", CreateTrainingCheckinSessionAPIView.as_view(),  name="create_training_checkin_session"),
     #endpoint para mobile, aluno registra presença via QR Code
-    path("api/v1/attendance/register/", views.RegisterAttendanceByQrAPIView.as_view(), name="attendance-register"),
+    path("api/v1/attendance/registerbyqrcode/", views.RegisterAttendanceByQrAPIView.as_view(), name="attendance-register"),
 ]
